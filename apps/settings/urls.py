3d01be6b94f04b655ae2e6ef15d10847a81f9371
Mohamed Wahba <devmohamedwahba@example.com>
2025-10-24 17:48:13 +0300

    settings app

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import ContactInfoViewSet
 
 urlpatterns = [
-    path('contact-info/', ContactInfoViewSet.as_view(), name='contact-info'),
+    path('contact-info/', ContactInfoViewSet.as_view({"get": "list"}), name='contact-info'),
 
 ]
