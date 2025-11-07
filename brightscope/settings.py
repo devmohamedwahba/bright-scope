4d4d6be5e8e9dd1580fad401c4cdcb30f5b20c17
Mohamed Wahba <devmohamedwahba@example.com>
2025-11-07 23:41:00 +0200

    add logo

--- brightscope/settings.py
+++ brightscope/settings.py
@@ -204,7 +204,7 @@ JAZZMIN_SETTINGS = {
     "site_header": "Bright Scope",
     "site_brand": "Bright Scope",
     "welcome_sign": "Welcome to Bright Scope Admin",
-    "login_logo": "logo.svg",  # Logo on login page
+    "login_logo": "img/logo.svg",  # Logo on login page
     "login_logo_dark": "logo.svg",  # Logo for dark mode (optional)
     "copyright": "Bright Scope Ltd",
 }
